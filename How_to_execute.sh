882d49b3f6f78f759fab2cb5d11b2a45772a4bd5
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-03-05 20:08:47 +0530

    How to run a .sh script file in Command line

--- How_to_execute.sh
+++ How_to_execute.sh
@@ -5,5 +5,5 @@
 #		or
 # 	./yourscript.sh
 
-# step 1 Commant give execute permission to your script
-# step 2 or choice (Since . refers to the current directory: if yourscript.sh is in the current directory, you can simplify this to:)
+# step 1 Command to give execute permission to your script
+# step 2 or choice (Since . refers to the current directory: if yourscript.sh is in the current directory, you can simplify it to .)
