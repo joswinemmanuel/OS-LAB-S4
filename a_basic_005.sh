19628fa39e71a6a698c4850f17d856a649f3676c
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-03-06 07:08:41 +0530

    Tutorial 5

---
+++ a_basic_005.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo Hello $1 and $2
+# ($1 represent the first argument provided in the executing command)
+# ($2 represent the second argument provided int the executing command)
+# Example :
+#   ./a_basic_005.sh JOSWIN Mileeex     (executing command with arguments JOSWIN, Mileeeex)
+#   Hello JOSWIN and Mileeex            (output)
+
+echo $1 EMMANULE 
+#   JOSWIN EMMANULE
+echo $2 SAAAAAN
+#   Mileeex SAAAAAN
