a4cb30b87eb8561947987556894a16a72f7ae5d3
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-03-06 07:28:15 +0530

    Tutorial 6

---
+++ a_basic_006.sh
@@ -0,0 +1,32 @@
+#1 /bin/bash
+
+echo "Enter your name : "
+read name
+echo WELCOME $name
+# Enter your name : 
+# Mileex
+# WELCOME Mileex
+
+echo "Enter names : "
+read name1 name2 name3
+echo WELCOME $name1, $name2, $name3
+# Enter names : 
+# POP TATE SHELBY
+# WELCOME POP, TATE, SHELBY
+
+# (To have the prompt on the same line)
+read -p 'Enter the username : ' un
+echo HELLO USER $un
+# Enter the username : Joswin
+# HELLO USER Joswin
+
+read -p 'Enter the username : ' un
+read -sp 'Enter the password : ' pw     # (To make hidden entry in prompt)
+echo                                    # (echo ,to echo a blank line, to go to next line)
+echo HELLO USER $un
+echo Password entered is : $pw
+
+# Enter the username : POP_SMOKE
+# Enter the password : 
+# HELLO USER POP_SMOKE
+# Password entered is : woolife
