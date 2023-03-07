a9ff02322b06774e661396d21f30665b8c507ecf
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-03-07 16:10:53 +0530

    Basic program 1

---
+++ program1.sh
@@ -0,0 +1,19 @@
+read -p 'Enter the name : ' name
+read -p 'Enter the semester : ' semester
+read -p 'Enter the batch : ' batch
+read -p 'Enter the roll number : ' rollno
+read -p 'Enter the registeration number : ' regno
+echo "$name $semester $batch $rollno $regno"	
+
+# echo Enter the name :
+# read name
+# echo Enter the semester :
+# read semester
+# echo Enter the batch :
+# read batch
+# echo Enter the roll number :
+# read rollno
+# echo Enter the registeration number :
+# read regno
+# echo "$name $semester $batch $rollno $regno"	 	
+
