26b8565fff2067bd09b1cac45895bfd048f9ec6f
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-03-28 14:16:47 +0530

    FCFS Scheduling

---
+++ CPU Scheduling/First_Come_First_Serve.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+
+typedef struct {
+    int arrT;
+    int compT;
+    int burstT;
+    int tat;
+    int wT;
+} process;
+
+process pr[50],temp;
+
+int main() {
+    int n, i, j, com=0;
+    float avgTT, avgWT, tTT=0, tWT=0;
+    printf("Enter the number of process : ");
+    scanf("%d", &n);
+    printf("\n");
+    for(i=0; i<n; i++){
+        printf("Enter the arrival time of P%d : ",i);
+        scanf("%d", &pr[i].arrT);
+        printf("Enter the burst time of P%d : ",i);
+        scanf("%d", &pr[i].burstT);
+    }
+    
+    for(i=0;i<n;i++) {
+        for(j=0; j<n-i-1; j++){
+            if(pr[j].arrT > pr[j+1].arrT) {
+                temp = pr[j];
+                pr[j] = pr[j+1];
+                pr[j+1] = temp;
+            }
+        }
+    }
+    
+    for(i=0; i<n; i++) {
+        if(com < pr[i].arrT){
+            com = pr[i].arrT;
+        }
+        pr[i].compT = pr[i].burstT+com;
+        pr[i].tat = pr[i].compT-pr[i].arrT;
+        pr[i].wT = pr[i].tat-pr[i].burstT;
+        com = pr[i].compT;
+    }
+    printf("\nprocess\tarr\ttt\twt\n");
+    for(i=0;i<n;i++) {
+        printf("P%d\t%d\t%d\t%d\n", i, pr[i].arrT, pr[i].tat, pr[i].wT);
+        tTT += pr[i].tat;
+        tWT += pr[i].wT;
+    }
+    avgTT = tTT/n;
+    avgWT = tWT/n;
+    printf("\nThe average waiting time is : %.2f \n", avgWT);
+    printf("The average turn around time is : %.2f \n", avgTT);
+}
