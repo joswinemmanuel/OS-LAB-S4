711e3232ae6c2206afc4bf75742eabcf48512214
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-06-20 16:04:07 +0530

    C Scan Disk Scheduling

--- Disk Scheduling/c_scan.c
+++ Disk Scheduling/c_scan.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-    int i, init, np, dir, temp, size, totalDisk, req[50];
+    int i, init, np, temp, size, totalDisk, req[50];
     printf("Enter the number of process : ");
     scanf("%d", &np);
     printf("Enter the request sequence : ");
@@ -14,8 +14,7 @@ int main() {
     size -= 1;
     printf("Enter the initial disk position : ");
     scanf("%d", &init);
-    printf("Enter direction of head (0 : left to right, 1 : right to left) : ");
-    scanf("%d", &dir);
+
     for(i=0; i<np; i++){
         for(int j=0; j<np-i-1; j++){
             if(req[j] > req[j+1]){
@@ -34,11 +33,7 @@ int main() {
         }
     }
     
-    if(!dir){
-        totalDisk = (size-init)+(point)+size;
-    } else {
-        totalDisk = (init+req[np-1]);
-    }
+    totalDisk = (size-init)+(point)+size;
     
     printf("The total disk movement : %d\n", totalDisk);
     return 0;
@@ -49,6 +44,5 @@ Enter the number of process : 7
 Enter the request sequence : 82 170 43 140 24 16 190
 Enter disk size : 200
 Enter the initial disk position : 50
-Enter direction of head (0 : left to right, 1 : right to left) : 0
 The total disk movement : 391
 */
