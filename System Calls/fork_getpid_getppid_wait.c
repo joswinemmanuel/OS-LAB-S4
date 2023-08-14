f4810e8df71a82e6affea9d429091371985a25a7
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-08-14 21:13:14 +0530

    program 1

--- System Calls/fork_getpid_getppid_wait.c
+++ System Calls/fork_getpid_getppid_wait.c
@@ -1,28 +1,29 @@
-#include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
 
 int main() {
-	int pid;
-	pid = fork();
-	
-	if(pid<0) {
-		fprintf(stderr, "Fork failed");
+	int id = fork();
+	if(id>0) {
+		wait(NULL);
+		printf("Parent process running\n");
+		printf("Parent id : %d\n", getpid());
 	}
-	else if(pid==0) {
+	else if(id==0) {
+		printf("Child process running\n");
 		printf("Child id : %d\n", getpid());
-		printf("Parent id : %d\n", getppid());
+		printf("Parent id : %d\n\n", getppid());
 	}
 	else {
-		wait(NULL);
-		printf("Parent present\n");
-		printf("Parent id : %d\n", getpid());
+		printf("Error\n");
+		printf("Exiting");
 	}
 }
 
 /*
-Child id : 13960
-Parent id : 13959
-Parent present
-Parent id : 13959
+Child process running
+Child id : 881
+Parent id : 880
+
+Parent process running
+Parent id : 880
 */
