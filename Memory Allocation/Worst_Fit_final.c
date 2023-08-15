552f8d49b58b551ee90dfff5686eba3266682df3
Joswin Emmanuel <96587885+joswinemmanuel@users.noreply.github.com>
2023-08-15 15:28:28 +0530

    Worst Fit final code

---
+++ Memory Allocation/Worst_Fit_final.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+int findpos(int x);
+int blocks[100], jobs[100], no_of_block, no_of_job;
+
+int main() {
+	printf("Enter the number of blocks : ");
+	scanf("%d", &no_of_block);
+	printf("Enter the number of jobs : ");
+	scanf("%d", &no_of_job);
+
+	int visited[no_of_job];
+
+	printf("Enter the size of the blocks : ");
+	for(int i=0; i<no_of_block; i++) {
+		scanf("%d", &blocks[i]);
+	}
+	printf("Enter the size of the jobs : ");
+	for(int i=0; i<no_of_job; i++) {
+		scanf("%d", &jobs[i]);
+		visited[i] = 0;
+	}
+
+	for(int i=0; i<no_of_job; i++) {
+		int j = findpos(jobs[i]);
+		if(j != 100) {
+			visited[i] = blocks[j];
+			blocks[j] -= jobs[i];
+		}
+	}
+
+	printf("\nSTATUS\n");
+	for(int i=0; i<no_of_job; i++) {
+		if(visited[i] == 0)
+			printf("%d Job is waiting\n", jobs[i]);
+		else
+			printf("%d Job is allocated to block %d\n", jobs[i], visited[i]);
+	}
+}
+
+int findpos(int x) {
+	int largest=0, found=0, index=0;
+	for(int i=0; i<no_of_block; i++) {
+		if(blocks[i] >= x) {
+			if(blocks[i] > largest) {
+				found = 1;
+				largest = blocks[i];
+				index = i;
+			}
+		}
+	}
+	if(found == 0)
+		return 100;
+	else
+		return index;
+}
+
+/*
+Enter the number of blocks : 5
+Enter the number of jobs : 4
+Enter the size of the blocks : 161 175 500 320 400
+Enter the size of the jobs : 75 140 200 301
+
+STATUS
+75 Job is allocated to block 500
+140 Job is allocated to block 425
+200 Job is allocated to block 400
+301 Job is allocated to block 320
+*/
